fcf53c214b60968cabe90026c179924853522112
veepra <veeralakshmi@gmail.com>
2018-01-19 11:50:11 -0500

    checkpoint 27

--- scripts/landing.js
+++ scripts/landing.js
@@ -28,8 +28,10 @@
     revealThirdPoint();
 
 };*/
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+
 var revealPoint =function(index) {
 
 
@@ -43,3 +45,15 @@ for(i=0;i<points.length; i++){
   revealPoint(i);
 }
 };
+window.onload = function() {
+  if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+ });
+ }
